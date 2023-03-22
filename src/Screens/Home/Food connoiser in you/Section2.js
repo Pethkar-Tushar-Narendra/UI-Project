b57224053c712e7c103b8fbf2eb188a76fded09c
Tushar Pethkar <pethkartusharnarendra@gmail.com>
2023-03-23 02:57:25 +0530

    23-3-2023

---
+++ src/Screens/Home/Food connoiser in you/Section2.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Section2.css';
+const Section2 = () => {
+  return (
+    <div className="section2-container">
+      <div className="section2-title">
+        <i className="icon"></i>
+        <span className="title"></span>
+      </div>
+    </div>
+  );
+};
+
+export default Section2;
