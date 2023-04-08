6a1dce6185556746792bf1cabff2de4eca73f591
Tushar Pethkar <pethkartusharnarendra@gmail.com>
2023-04-08 12:32:32 +0530

    commit

--- src/Screens/Home/Food connoiser in you/Section2.js
+++ src/Screens/Home/Food connoiser in you/Section2.js
@@ -7,6 +7,25 @@ const Section2 = () => {
         <i className="icon"></i>
         <span className="title"></span>
       </div>
+      <div className="dishes-container">
+        <div className="row1">
+          <div className="dish1"></div>
+          <div className="dish2"></div>
+          <div className="dish3"></div>
+          <div className="dish4"></div>
+          <div className="dish5"></div>
+          <div className="dish6"></div>
+        </div>
+        <div className="row2">
+          <div className="dish1"></div>
+          <div className="dish2"></div>
+          <div className="dish3"></div>
+          <div className="dish4"></div>
+          <div className="dish5"></div>
+          <div className="dish6"></div>
+          <div className="dish7"></div>
+        </div>
+      </div>
     </div>
   );
 };
